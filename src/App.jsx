d91ce62078db39aa417db30d7f50c550e714c369
dev-imani <michaelimani396@gmail.com>
2025-04-18 12:30:55 +0300

    my second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
 
 function App() {
   return (
-    <div>
-      <h1>I am Imani Michael</h1>
-    </div>
+<>
+<h1>I am Imani Michael</h1>
+      <FavoriteColor/>
+</>
+      
+
   )
+}
+
+function FavoriteColor() {
+  const [color, setColor] = useState("red");
+
+  return <h1 style={{color:"red"}}>My favorite color is {color}!</h1>
 }
 
 export default App
