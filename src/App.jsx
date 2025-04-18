56eb7f1ed4da2695272246249cabafa592c0aebc
dev-imani <michaelimani396@gmail.com>
2025-04-18 12:42:47 +0300

    finshed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
 <>
 <h1>I am Imani Michael</h1>
       <FavoriteColor/>
+      <FavoriteColor2/>
+      <Car/>
+      <Car1/>
+      <Car3/>
 </>
       
 
@@ -17,5 +21,85 @@ function FavoriteColor() {
 
   return <h1 style={{color:"red"}}>My favorite color is {color}!</h1>
 }
+
+
+function FavoriteColor2() {
+  const [color, setColor] = useState("red");
+
+  return (
+    <>
+      <h1>My favorite color is {color}!</h1>
+      <button
+        type="button"
+        onClick={() => setColor("blue")}
+      >Blue</button>
+    </>
+  )
+}
+
+function Car() {
+  const [brand, setBrand] = useState("Ford");
+  const [model, setModel] = useState("Mustang");
+  const [year, setYear] = useState("1964");
+  const [color, setColor] = useState("red");
+
+  return (
+    <>
+      <h1>My {brand}</h1>
+      <p>
+        It is a {color} {model} from {year}.
+      </p>
+    </>
+  )
+}
+
+
+function Car1() {
+  const [car, setCar] = useState({
+    brand: "Ford",
+    model: "Mustang",
+    year: "1964",
+    color: "red"
+  });
+
+  return (
+    <>
+      <h1>My {car.brand}</h1>
+      <p>
+        It is a {car.color} {car.model} from {car.year}.
+      </p>
+    </>
+  )
+}
+
+function Car3() {
+  const [car, setCar] = useState({
+    brand: "Ford",
+    model: "Mustang",
+    year: "1964",
+    color: "red"
+  });
+
+  const updateColor = () => {
+    setCar(previousState => {
+      return { ...previousState, color: "blue" }
+    });
+  }
+
+  return (
+    <>
+      <h1>My {car.brand}</h1>
+      <p>
+        It is a {car.color} {car.model} from {car.year}.
+      </p>
+      <button
+        type="button"
+        onClick={updateColor}
+      >Blue</button>
+    </>
+  )
+}
+
+
 
 export default App
